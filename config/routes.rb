d523146ebd0c293563e2c9a137cb234ca5027488
alatif09 <146213608+alatif09@users.noreply.github.com>
2023-12-09 16:05:37 +0000

    final

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,18 @@
 Rails.application.routes.draw do
 
+  get("/", {:controller => "users", :action => "home"})
+
+  get("/users", {:controller => "users", :action => "index"})
+  post("/add_new_user", {:controller => "users", :action => "add"})
+  get("users/:username", {:controller => "users", :action =>"show"})
+  post("/update_user_details/:old_username", {:controller => "users", :action => "modify"})
+
+  get("/photos", {:controller => "photos", :action => "index"})
+  post("/add_new_photo", {:controller => "photos", :action => "add"})
+  get("/photos/:photo_id", {:controller=> "photos", :action => "show"})
+  post("/update_photo/:photo_id", {:controller => "photos", :action =>"modify"})
+  get("/delete_photo/:photo_id", {:controller => "photos", :action => "delete"})
+
+  post("/add_new_comment", {:controller => "comments", :action => "add"})
+
 end
